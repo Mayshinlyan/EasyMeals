118a5bf494639d40943df7a363de4b9866e960a6
May Shin Lyan <hwai@u.rochester.edu>
2024-09-17 13:18:07 -0700

    Added PDF meal plan generation with Dalle 3 image integration

--- src/data/environment_variables.py
+++ src/data/environment_variables.py
@@ -5,8 +5,8 @@ from dotenv import find_dotenv, load_dotenv
 # project_dir = os.path.join(os.path.dirname(__file__), os.pardir)
 # dotenv_path = os.path.join(project_dir, '.env')
 
-
 dotenv_path = "/Users/maylyan/Documents/AI/Project 01/.env"
 load_dotenv(dotenv_path)
-API_KEY = os.getenv("API_KEY")
+
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 PASSWORD = os.getenv("PASSWORD")
